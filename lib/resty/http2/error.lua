5b2dc9242ea4d85510b383572db2569f4f46482e
Alex Zhang <zchao1995@gmail.com>
2018-08-29 13:26:48 +0800

    improve: refactored error.lua

--- lib/resty/http2/error.lua
+++ lib/resty/http2/error.lua
@@ -2,34 +2,43 @@
 
 local _M = { _VERSION = "0.1" }
 
-_M.NO_ERROR = 0x0
-_M.PROTOCOL_ERROR = 0x1
-_M.INTERNAL_ERROR = 0x2
-_M.FLOW_CONTROL_ERROR = 0x3
-_M.SETTINGS_TIMEOUT = 0x4
-_M.STREAM_CLOSED = 0x5
-_M.FRAME_SIZE_ERROR = 0x6
-_M.REFUSED_STREAM = 0x7
-_M.CANCEL = 0x8
-_M.COMPRESSION_ERROR = 0x9
-_M.CONNECT_ERROR = 0xa
-_M.ENHANCE_YOUR_CALM = 0xb
-_M.INADEQUATE_SECURITY = 0xc
-_M.HTTP_1_1_REQUIRED = 0xd
-
-_M.INVALID_STREAM_STATE = 1
-_M.FLOW_EXHAUSTED = 2
-_M.STREAM_OVERFLOW = 3
-
+local NO_ERROR = 0x0
+local PROTOCOL_ERROR = 0x1
+local INTERNAL_ERROR = 0x2
+local FLOW_CONTROL_ERROR = 0x3
+local SETTINGS_TIMEOUT = 0x4
+local STREAM_CLOSED = 0x5
+local FRAME_SIZE_ERROR = 0x6
+local REFUSED_STREAM = 0x7
+local CANCEL = 0x8
+local COMPRESSION_ERROR = 0x9
+local CONNECT_ERROR = 0xa
+local ENHANCE_YOUR_CALM = 0xb
+local INADEQUATE_SECURITY = 0xc
+local HTTP_1_1_REQUIRED = 0xd
 
 -- we use negative codes to represent the some stream-level errors
-_M.STREAM_PROTOCOL_ERROR = - _M.PROTOCOL_ERROR
-_M.STREAM_FLOW_CONTROL_ERROR = - _M.FLOW_CONTROL_ERROR
+local STREAM_PROTOCOL_ERROR = -PROTOCOL_ERROR
+local STREAM_FLOW_CONTROL_ERROR = -FLOW_CONTROL_ERROR
 
 local error_map = {
-    [_M.INVALID_STREAM_STATE] = "invalid stream state",
-    [_M.FLOW_EXHAUSTED] = "flow window is exhausted",
-    [_M.STREAM_OVERFLOW] = "concurrent streams exceeds",
+    [NO_ERROR] = "no error",
+    [PROTOCOL_ERROR] = "protocol error",
+    [INTERNAL_ERROR] = "internal error",
+    [FLOW_CONTROL_ERROR] = "flow control error",
+    [SETTINGS_TIMEOUT] = "settings timeout",
+    [STREAM_CLOSED] = "stream closed",
+    [FRAME_SIZE_ERROR] = "frame size error",
+    [REFUSED_STREAM] = "refused stream",
+    [CANCEL] = "cancel",
+    [COMPRESSION_ERROR] = "compression error",
+    [CONNECT_ERROR] = "connect error",
+    [ENHANCE_YOUR_CALM] = "enhanced your calm",
+    [INADEQUATE_SECURITY] = "inadequate security",
+    [HTTP_1_1_REQUIRED] = "http/1.1 required",
+
+    [STREAM_PROTOCOL_ERROR] = "protocol error (stream level)",
+    [STREAM_FLOW_CONTROL_ERROR] = "flow control error (stream level)",
 }
 
 
@@ -43,8 +52,27 @@ function _M.is_stream_error(code)
         return true
     end
 
-    return code == _M.REFUSED_STREAM or code == _M.STREAM_CLOSED
+    return code == REFUSED_STREAM or code == STREAM_CLOSED
 end
+
+
+_M.NO_ERROR = NO_ERROR
+_M.PROTOCOL_ERROR = PROTOCOL_ERROR
+_M.INTERNAL_ERROR = INTERNAL_ERROR
+_M.FLOW_CONTROL_ERROR = FLOW_CONTROL_ERROR
+_M.SETTINGS_TIMEOUT = SETTINGS_TIMEOUT
+_M.STREAM_CLOSED = STREAM_CLOSED
+_M.FRAME_SIZE_ERROR = FRAME_SIZE_ERROR
+_M.REFUSED_STREAM = REFUSED_STREAM
+_M.CANCEL = CANCEL
+_M.COMPRESSION_ERROR = COMPRESSION_ERROR
+_M.CONNECT_ERROR = CONNECT_ERROR
+_M.ENHANCE_YOUR_CALM = ENHANCE_YOUR_CALM
+_M.INADEQUATE_SECURITY = INADEQUATE_SECURITY
+_M.HTTP_1_1_REQUIRED = HTTP_1_1_REQUIRED
+
+_M.STREAM_PROTOCOL = STREAM_PROTOCOL_ERROR
+_M.STREAM_FLOW_CONTROL_ERROR = STREAM_FLOW_CONTROL_ERROR
 
 
 return _M
